c2334955ae2691b70a4bbf7d87e6671aebe41951
Aidan Melville <amelville@lcs.on.ca>
2021-01-14 11:47:10 -0500

    Using closures as parameters when they accept parameters video 1 day 7

--- Notes/Closures Playground..playground/Contents.swift
+++ Notes/Closures Playground..playground/Contents.swift
@@ -80,3 +80,34 @@ travel {
 
 
 
+
+
+
+//Day 7 Part 2 of closures
+
+//The least you need to know
+
+
+//Video 1
+//Using closures as parameters when they accept parameters
+
+//We’ve been using () -> Void to mean “accepts no parameters and returns nothing”, but you can go ahead and fill the () with the types of any parameters that your closure should accept.
+
+//To demonstrate this, we can write a travel() function that accepts a closure as its only parameter, and that closure in turn accepts a string
+
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+
+//Now when we call travel() using trailing closure syntax, our closure code is required to accept a string.
+travel { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+
+
+// Video 2
+//
+
