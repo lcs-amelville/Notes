d37baf37cc7af7adf84c5a88d645263a9fc88c37
Aidan Melville <amelville@lcs.on.ca>
2021-01-18 14:35:33 -0500

    Day 8 Viedo 3 and 4 structures. Property observers and methods.

--- Structures Playground..playground/Contents.swift
+++ Structures Playground..playground/Contents.swift
@@ -37,4 +37,48 @@ struct SPort {
 let chessBoxing = SPort(name: "Chessboxing", isOlympicSport: false)
 print(chessBoxing.olympicStatus)
 
-//
+
+
+/// Video 3 Property observers
+//Property observers let you run code before or after any property changes
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+//We can now create an instance of that struct and adjust its progress over time
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+//What we want to happen is for Swift to print a message every time amount changes, and we can use a didSet property observer for that.
+//You can also use willSet to take action before a property changes, but that is rarely used
+
+
+
+/// Video 4
+//Structs can have functions inside them, and those functions can use the properties of the struct as they need to. Functions inside structs are called methods
+
+struct City {
+    var population: Int
+
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+//That method belongs to the struct, so we call it on instances of the struct like this
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+
+
+/// Video 5
+
+
